8761c4aba97aecdde3f9235b7560f0b28dd846de
Azury <azury.wtf@gmail.com>
2021-11-24 18:36:28 +0100

    removed old files

--- src/cdn/index.js
+++
@@ -1,9 +0,0 @@
-function widget(config) {
-  if (typeof config !== 'object') return
-
-  // attributes
-  const theme = config.theme ?? 'dark'
-  const placement = config.placement ?? 'bottom right'
-  const uploadLimit = config.uploadLimit ?? 20
-  const zIndex = config.zIndex ?? 9999
-}
