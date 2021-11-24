e8de6b3b8387c1bc85d8d865ffc7d785c580a73f
Azury <azury.wtf@gmail.com>
2021-11-24 18:37:32 +0100

    added types

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+interface Configuration {
+    theme?: String;
+    placement?: String;
+    uploadLimit?: Number;
+    zIndex?: Number;
+}
+declare const _default: (config?: Configuration | undefined) => void;
+export default _default;
