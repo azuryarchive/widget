baf80525f73cf096e0d964d6432f3aa33b0e289a
Azury <azury.wtf@gmail.com>
2021-12-04 21:28:07 +0100

    finalized main file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 // config
-import { version } from './config.json'
+import { version } from '../package.json'
 
 // modules
-import error from './modules/error'
-import checkStatus from './modules/checkStatus'
 import togglePopup from './modules/togglePopup'
-import upload from './modules/upload'
 import generatePopup from './modules/generatePopup'
 
 // styles
@@ -61,7 +58,7 @@ export default (config?: Configuration) => {
   container.className = `azury_widget azury_theme_${theme}`
 
   // prepare icon
-  icon.onclick = togglePopup
+  icon.onclick = () => togglePopup(uploadLimit)
   icon.className = 'azury_icon'
   icon.innerHTML = '<i class="material-icons-round">note_add</i>'
   container.appendChild(icon)
@@ -75,6 +72,19 @@ export default (config?: Configuration) => {
   // customize zIndex
   icon.style.zIndex = zIndex.toString()
 
+  // get initial icon content
+  let initialIconContent = icon.innerHTML
+
+  // display version on right click
+  icon.addEventListener('auxclick', () => {
+    if (icon.innerHTML === `<p>v${version}</p>`) {
+      icon.innerHTML = initialIconContent
+    } else {
+      initialIconContent = icon.innerHTML
+      icon.innerHTML = `<p>v${version}</p>`
+    }
+  })
+
   // generate popup
   container.appendChild(generatePopup({
     placement: placement,
@@ -84,30 +94,4 @@ export default (config?: Configuration) => {
 
   // append widget
   document.body.appendChild(container)
-
-  // handle upload
-  document.querySelector('.azury_upload')?.addEventListener('click', () => {
-    const input: any = document.createElement('input')
-    input.type = 'file'
-    input.click()
-
-    input.addEventListener('change', async () => {
-      popup.innerHTML = `
-        <div class="azury_main_wrapper">
-          <div class="azury_main">
-            <i class="material-icons-round loading">sync</i>
-            <span>Uploading...</span>
-          </div>
-        </div>
-        <footer>
-          <a href='https://github.com/azurystudios/widget/blob/main/terms_of_service.md' target='_blank' rel='noreferrer'>TOS</a>
-          <p>- Powered by <a href='https://azury.gg' target='_blank' rel='noreferrer'>Azury</a></p>
-        </footer>
-      `
-      
-      //const response = await upload(input.files[0], options.uploadLimit)
-      // uploadIcon.className = 'material-icons-round azury_upload'
-      // uploadIcon.innerHTML = 'file_upload'
-    })
-  })
 }
