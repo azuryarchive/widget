42491bfb43f35fe4c7acec18a8f4cda0df611bf2
Azury <azury.wtf@gmail.com>
2021-12-11 15:12:26 +0100

    fixed popup toggling

--- src/modules/togglePopup.ts
+++ src/modules/togglePopup.ts
@@ -1,33 +1,26 @@
 import handleUpload from './handleUpload'
+import { uploadLimit } from '../config.json'
 import styles from '../styles.module.css'
+import changeContent from './changeContent'
 
-export default (uploadLimit: number) => {
-  const popup = document.querySelector(`.${styles.popup}`)
+export default () => {
+  changeContent(`
+    <div class=${styles.popupContent}>
+      <h1>Upload File</h1>
+      <i class='${styles.uploadIcon} material-icons-outlined'>file_upload</i>
+      <span>Max. ${uploadLimit} MB</span>
+    </div>
+  `)
+
+  document.querySelector(`.${styles.widget}`)?.classList.toggle(styles.widgetActive)
+  document.querySelector(`.${styles.uploadIcon}`)?.addEventListener('click', handleUpload)
+
   const icon = document.querySelector(`.${styles.icon}`)
-  const uploadIcon = document.querySelector(`.${styles.uploadIcon}`)
-
-  if (!popup || !icon || !uploadIcon) return
-  
-  if (window.getComputedStyle(popup).display === 'none') {
-    icon.innerHTML = '<i class=\'material-icons-round\'>close</i>'
-    uploadIcon.addEventListener('click', () => handleUpload(uploadLimit))
+  if (!document.querySelector(`.${styles.widgetActive}`)) {
+    // popup is opened
+    if (icon) icon.innerHTML = `<i class='material-icons-round'>note_add</i>`
   } else {
-    if (popup) popup.innerHTML = `
-      <div class=${styles.popupContentWrapper}>
-        <div class=${styles.popupContent}>
-          <h1>Upload File</h1>
-          <i class="${styles.uploadIcon + ' material-icons-outlined'}">file_upload</i>
-          <span>Max. ${uploadLimit} MB</span>
-        </div>
-      </div>
-      <footer>
-        <a href='https://github.com/azurystudios/widget/blob/main/terms_of_service.md' target='_blank' rel='noreferrer'>TOS</a>
-        <p>- Powered by <a href='https://azury.gg' target='_blank' rel='noreferrer'>Azury</a></p>
-      </footer>
-    `
-    icon.innerHTML = '<i class=\'material-icons-round\'>note_add</i>'
+    // popup is closed
+    if (icon) icon.innerHTML = `<i class='material-icons-round'>close</i>`
   }
-  
-  popup.classList.toggle(styles.popupActive)
-  icon.classList.toggle(styles.iconActive)
 }
