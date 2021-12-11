4d37f76d75af2772f679909e6695aa8dc9f85c34
Azury <azury.wtf@gmail.com>
2021-12-11 15:13:07 +0100

    enhanced error handler

--- src/modules/error.ts
+++ src/modules/error.ts
@@ -1,19 +1,9 @@
-import styles from '../styles.module.css'
+import changeContent from './changeContent'
 
 export default (message: string) => {
-  const popup = document.querySelector(`.${styles.popup}`)
-
-  if (popup) popup.innerHTML = `
-    <div class=${styles.popupContentWrapper}>
-      <div class="${styles.popupContent + ` ${styles.error}`}">
-        <i class='material-icons-round'>error_outline</i>
-        <p>${message.toUpperCase()}</p>
-        <small>Please reopen the popup!</small>
-      </div>
-    </div>
-    <footer>
-      <a href='https://github.com/azurystudios/widget/blob/main/terms_of_service.md' target='_blank' rel='noreferrer'>TOS</a>
-      <p>- Powered by <a href='https://azury.gg' target='_blank' rel='noreferrer'>Azury</a></p>
-    </footer>
-  `
+  changeContent(`
+    <i class='material-icons-round'>error_outline</i>
+    <p>${message.toUpperCase()}</p>
+    <small>Please reopen the popup!</small>
+  `, true)
 }
